676ac488db148fe4c813e0983e862f37a9ca2264
Alexander Spivey <alexander.spivy@gmail.com>
2021-10-23 19:21:27 -0500

    Update Trees.java

--- CSE2/Assignments/Assignment01/Trees.java
+++ CSE2/Assignments/Assignment01/Trees.java
@@ -9,7 +9,7 @@ public class Trees{
     System.out.println("     /#|#\\          /#|#\\     ");
     System.out.println("    /##|##\\        /##|##\\    ");
     System.out.println("   /###|###\\      /###|###\\   ");
-    System.out.println("       |              |       ");
-    System.out.println("       |              |       ");
+    System.out.println("       |               |       ");
+    System.out.println("       |               |       ");
   }
 }
